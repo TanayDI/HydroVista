3a8db73e8003a62a9330ca618cfd633709cc17b8
TanayDI <94367529+TanayDI@users.noreply.github.com>
2024-11-29 02:28:22 +0530

    Update ip.py

--- api/src/ip.py
+++ api/src/ip.py
@@ -1,5 +1,5 @@
 import requests
-url = "https://susya.onrender.com"
+url = "https://hydrovista.onrender.com"
 #imgdata = base64 string of image
 r = requests.post(url,json = {"image":imgdata})
 print(r.text.strip())
