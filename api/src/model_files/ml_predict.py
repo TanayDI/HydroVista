56c0f2393f8f208690e091878ef9a71595995c2e
TanayDI <94367529+TanayDI@users.noreply.github.com>
2024-11-29 08:16:30 +0530

    Update ml_predict.py

--- api/src/model_files/ml_predict.py
+++ api/src/model_files/ml_predict.py
@@ -81,7 +81,7 @@ def predict_plant(model, imgdata):
         labels = pickle.load(lb)
 
     loaded_model = model
-    loaded_model.load_state_dict(torch.load("model_2.pth"))
+    loaded_model.load_state_dict(torch.load("model_files/model_2.pth"))
     loaded_model.eval()
 
     # Converting Base64 string to Image
